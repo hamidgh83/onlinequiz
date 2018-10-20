535e23871def009e91004a5279b5401a926993c0
Hamid Ghorashi <hamidghorashi@hyperoffice.com>
2018-10-20 21:07:24 +0330

    Install bootstrap for the project

---
+++ src/Assets/README.md
@@ -0,0 +1,10 @@
+equirements
+This project requires **npm** to be installed. 
+
+### Installation
+This project uses bootstrap v3.3.7. In order to install bootstrap just go to **Assest** folder and update the project using the following command:
+
+```sh
+$ npm update
+```
+
--- src/View/template/default.php
+++ src/View/template/default.php
@@ -5,11 +5,12 @@
         <title>Online Quiz</title>
         <meta name="This is a simple MVC application to generate an online quiz" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="Assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
     </head>
     <body>
     
         <?php echo $content ?> 
     
+        <script type="text/javascript" src="Assets/node_modules/bootstrap/dist/js/bootstrap.min.js">
     </body>
 </html>
