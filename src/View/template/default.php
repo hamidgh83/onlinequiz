8aade76d25dd9e33858f1fcfa9e2f610bf2560f7
Hamid Ghorashi <hamidghorashi@hyperoffice.com>
2018-10-21 10:04:16 +0330

    Design questions view pages

--- src/View/template/default.php
+++ src/View/template/default.php
@@ -5,12 +5,18 @@
         <title>Online Quiz</title>
         <meta name="This is a simple MVC application to generate an online quiz" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="Assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="Assets/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="Assets/css/style.css" />
     </head>
     <body>
+
+        <section class="container">
+            <article class="content-wraper">
+                <?php echo $content ?> 
+            </article>
+        </section>
     
-        <?php echo $content ?> 
-    
-        <script type="text/javascript" src="Assets/node_modules/bootstrap/dist/js/bootstrap.min.js">
+        <script type="text/javascript" src="Assets/node_modules/jquery/dist/jquery.min.js" />
+        <script type="text/javascript" src="Assets/node_modules/bootstrap/dist/js/bootstrap.min.js" />
     </body>
 </html>
